4fcb9dad5450034a538969688adccdc229c873b6
Mark Boorer <markboo99@gmail.com>
2012-11-21 09:15:27 +1100

    Added BorderedSquare

--- python/py2dgui/__init__.py
+++ python/py2dgui/__init__.py
@@ -1,3 +1,3 @@
-from py2dgui.primitives import Triangle, Square
+from py2dgui.primitives import Triangle, Square, BorderedSquare
 from py2dgui.stage import Stage
 from py2dgui.base import Point, Color
