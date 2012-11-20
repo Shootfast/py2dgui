19f7962d1c7ec93bc8d7819d5d08f2f492f0d4c1
Mark Boorer <markboo99@gmail.com>
2012-11-21 09:26:01 +1100

    Changed default stage size to 1 to avoid div by zero, fixed projection matrix, added matrixStack, removed frameInfo object

--- python/py2dgui/stage.py
+++ python/py2dgui/stage.py
@@ -1,7 +1,17 @@
 from py2dgui.shaders import vertex, fragment
-from OpenGL.GL import   shaders,			 \
-						GL_VERTEX_SHADER,	\
-						GL_FRAGMENT_SHADER
+from py2dgui.base import MatrixStack
+
+from OpenGL.GL import   shaders,			    \
+						GL_TRUE,                \
+						GL_VERTEX_SHADER,	    \
+						GL_FRAGMENT_SHADER,     \
+						GL_BLEND,               \
+						GL_SRC_ALPHA,           \
+						GL_ONE_MINUS_SRC_ALPHA, \
+						glEnable,               \
+						glBlendFunc,            \
+						glGetUniformLocation,   \
+						glUniformMatrix4fv
 						
 from time import time
 
@@ -9,16 +19,17 @@ from numpy import array
 
 class Stage(object):
 	'''
-	Main object from which all other RVPY2 objects are rendered
+	Main object from which all other py2dgui objects are rendered
 	'''
-	def __init__(self, width=0, height=0):
-		self.width = 0
-		self.height = 0
+	def __init__(self, width=1, height=1):
+		self.width = 1
+		self.height = 1
 		
 		self.actors = []
 		
 		self.shader = None
 		self.projection_matrix = None
+		self.modelCamera_matrix = MatrixStack()
 		
 		# Time the stage was constructed
 		self.startTime = time()
@@ -31,14 +42,19 @@ class Stage(object):
 		
 	def setup(self):
 		'''
-		Compile the shader program
+		Compile the shader programs
 		'''
+		
 		# Compile the Vertex Shader
 		VERTEX_SHADER = shaders.compileShader(vertex, GL_VERTEX_SHADER)
 		# Compile the Fragment Shader
 		FRAGMENT_SHADER = shaders.compileShader(fragment, GL_FRAGMENT_SHADER)
 		# Join them as the shader program
 		self.shader = shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
+		
+		# Ensure that transparency is enabled 
+		glEnable(GL_BLEND)
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 		
 		
 	def getElapsedTime(self):
@@ -47,34 +63,24 @@ class Stage(object):
 		'''
 		return time() - self.startTime
 		
-	def resize(self, width, height):
+	def resize(self, w, h):
 		'''
 		Set the size of the stage
 		'''
-		self.width = width
-		self.height = height
 		
-		zNear = 0.5
-		zFar  = 3.0
+		self.width = w
+		self.height = h
 		
-		# recalculate Orthogonal projection matrix
+		n = 0.5 # zNear
+		f  = 3.0 # zFar
+		
 		self.projection_matrix = array([
 									
-				[2/float(width),        0,                0,         0          ],
-				[     0,         2/float(height),         0,         0          ],
-				[     0,                0,        1/(zFar - zNear), -zNear / (zFar - zNear)],
-				[     0,                0,      0,     1 ]
+				[ 2/float(w),     0,           0,            -1        ],
+				[     0,      2/float(h),      0,            -1        ],
+				[     0,          0,      -2/(f-n),          -1        ],
+				[     0,          0,           0,             1        ]
 										],'f')
-		
-		self.identity_matrix = array([
-									[1,0,0,0],
-									[0,1,0,0],
-									[0,0,1,0],
-									[0,0,0,1]
-									],'f')
-		
-		# Use the identity matrix till I can fix this :/
-		#self.projection_matrix = self.identity_matrix
 		
 		
 	def add_actor(self, actor):
@@ -83,20 +89,35 @@ class Stage(object):
 		'''
 		if actor not in self.actors:
 			self.actors.append(actor)
+			
+			
+	def clear_stage(self):
+		'''
+		Remove all actors from the stage
+		'''
+		self.actors = []
+		
 
 	def render(self):
 		'''
 		Render all actors on the stage
 		'''
 		before = time()
+				
+		# Load our shader
+		shaders.glUseProgram(self.shader)
 		
-		# Create a FrameInfo object
-		frameInfo = FrameInfo(self, self.shader, self.projection_matrix)
+		# Set the projectionMatrix uniform
+		projectionMatrixUniform = glGetUniformLocation(self.shader, 'projectionMatrix');
+		glUniformMatrix4fv(projectionMatrixUniform, 1, GL_TRUE, self.projection_matrix);
 		
 		# Render our actors
 		for actor in self.actors:
-			actor.render(frameInfo)
+			actor._render(self, self.modelCamera_matrix)
 		after = time()
+		
+		# Unload our shader
+		shaders.glUseProgram(0)
 		
 		# record the framerate
 		framerate = after - before
@@ -105,13 +126,3 @@ class Stage(object):
 		# Record frame finish time
 		self.frameFinishTime = after
 		
-
-class FrameInfo(object):
-	'''
-	Object that contains all information from stage needed to render an actor
-	'''
-	def __init__(self, stage, shader, projection_matrix):
-		self.stage = stage
-		self.frameFinishTime = stage.frameFinishTime
-		self.shader = shader
-		self.projection_matrix = projection_matrix
