99a3dc9388a84908dbda40f3495e5047f255926d
Mark Boorer <markboo99@gmail.com>
2012-11-22 11:30:26 +1100

    Created BaseActor class for Actors to inherrit. Created TextActor for text

--- python/py2dgui/actor.py
+++ python/py2dgui/actor.py
@@ -1,90 +1,68 @@
 from OpenGL.GL import   glDrawArrays,			      \
-						GL_TRIANGLES,			      \
-						glVertexAttribPointer,	      \
-						glEnableVertexAttribArray,    \
-						glDisableVertexAttribArray,   \
-						glGetAttribLocation,	      \
-						GL_FLOAT,				      \
-						glGetUniformLocation,         \
-						glUniformMatrix4fv,           \
-						GL_TRUE
-						
-						
+						glBufferData,                 \
+						GL_TRIANGLES,		          \
+						GL_ARRAY_BUFFER
+									
 from OpenGL.arrays import vbo   
 from numpy import array
 
 from py2dgui.base import Point, Color
-from math import sin, cos
+from py2dgui.atlas import Atlas
+
+
 
 
 						
-class Actor(object):
-	def __init__(self, points, colors=[], color=Color(0.5, 0.5, 0.5, 1.0)):
+class BaseActor(object):
+	'''
+	Base for all actor instances to build from
+	'''
+	
+	# Actortype (used to determine shader)
+	actortype = None
+	
+	def __init__(self):
 		
-		# list of points
-		self._points = points
-		# list of colors
-		self._colors = colors
-				
-		# Default color
-		self.default_color = color
+		# Place holder for VBO object
+		self.vbo = None
 		
 		# Is the VBO ready to render with current data
 		self.ready = False
 		
+		# Transformation values
 		self._translation = Point()
 		self._rotation = Point()		
 		self._scale = Point(1,1,1)
 		
+		# Matrix from model to camera (provided in _render)
+		self.matrixStack = None
+		self.modelCameraMatrix = None
+		
+		# Projection matrix
+		self.projection_matrix = None
+		
 		# Child actors
 		self.children = []
 		
-	
-	@property
-	def points(self):
-		'''
-		Get the verticies for this object
-		'''
-		return self._points
-	
-	@points.setter
-	def points(self, value):
-		'''
-		Set the verticies for this object
-		'''
-		self._points = value
-		self.ready = False
-		
-	@property
-	def colors(self):
-		'''
-		Get the colors for this object
-		'''
-		return self._colors
-
-	@colors.setter
-	def colors(self, value):
-		'''
-		Set the colors for this object
-		'''
-		self._colors = value
-		self.ready = False
+		# Safety flags
+		self.preRenderRan = False
+		self.postRenderRan = True
 		
 		
 	def translate(self, value):
-		self._translation += value
+		self._translation = value
 		
 	def getTranslation(self):
 		return self._translation
 		
 	def rotate(self, value):
-		self._rotation += value
+		self._rotation = value
 		
 	def getRotation(self):
 		return self._rotation
 	
 	def scale(self, value):
-		self._scale += value
+		self._scale = value
 	
 	def getScale(self):
 		return self._scale
@@ -97,11 +75,126 @@ class Actor(object):
 			self.children.remove(child)
 			
 		
-	def _updateVBO(self):
+	def _assignVBO(self, a):
 		'''
-		Update the data in the VBO for this object
+		Assign the data in the VBO for this object
 		'''
+			
+		# Assign VBO
+		self.vbo = vbo.VBO(
+						   array(a, 'f')
+						   )
+		# Setup is complete
+		self.ready = True 
+			
+	def _prerender(self, stage, projection_matrix, matrixStack):
+		'''
+		Sets up the OpenGL environment to render this actor
+		returns the shader used
+		'''
+		# Check flags
+		if self.postRenderRan != True:
+			raise Exception("_postrender was not called after previous _render")
+		# reset flag for postrender
+		self.postRenderRan = False
 		
+		# Check if this object has been setup yet
+		if not self.ready:
+			raise Exception("_render called before _assignVBO")
+				
+		# Push the current state of the matrix stack before we start changing things
+		self.matrixStack = matrixStack 
+		self.matrixStack.push()
+		
+		# Apply transformations
+		self.matrixStack.translate(self.getTranslation())
+		self.matrixStack.rotatex(self.getRotation().x)
+		self.matrixStack.rotatey(self.getRotation().y)
+		self.matrixStack.rotatez(self.getRotation().z)
+		self.matrixStack.scale(self.getScale())	
+		
+		# Calculate the camera2model matrix for this actor
+		self.modelCamera_matrix = self.matrixStack.top()
+		
+		# Assign the projection matrix
+		self.projection_matrix = projection_matrix
+		
+		# Get the correct shader from the stage
+		shader = stage.getShader(self.actortype)
+		
+		# Bind the VBO 
+		self.vbo.bind()
+		
+		# Run the setup for the shader
+		shader.setup(self)
+		
+		# set this flag to say the prerender ran
+		self.preRenderRan = True
+		
+		return shader
+
+
+	def _render(self, stage, projection_matrix, modelCamera_matrix):
+		'''
+		Render this Actor
+		(should be overridden by child classes)
+		'''
+		raise NotImplementedError
+		
+		
+	def _postrender(self, stage, shader):
+		'''
+		Cleans up the OpenGL environment after rendering this actor
+		'''
+		#Check flags
+		if self.preRenderRan == False:
+			raise Exception("_prerender was not called before _render")
+		# reset flag
+		self.preRenderRan = False			
+	
+		shader.cleanup()
+		
+		self.vbo.unbind()	
+			
+		# Render children
+		for child in self.children:
+			child._render(stage, self.projection_matrix, self.matrixStack)
+			
+		# Pop the matrix stack back to what it was
+		self.matrixStack.pop()
+		
+		# Set this flag to say the postrender ran
+		self.postRenderRan = True
+
+
+
+
+class PrimitiveActor(BaseActor):
+	'''
+	Actor for primitive shapes
+	'''
+	# Actortype (used to determine shader)
+	actortype = 'primitive'
+	
+	def __init__(self, points, colors=[], color=Color(0.5, 0.5, 0.5, 1.0)):
+		super(PrimitiveActor, self).__init__()
+		
+		# list of points
+		self.points = points
+		# list of colors
+		self.colors = colors
+				
+		# Default color
+		self.default_color = color
+		
+		# Assign the VBO
+		self._assignVBO()
+		
+
+	def _assignVBO(self):
+		'''
+		Assign the data in the VBO for this object
+		'''
 		# array to build for vbo 
 		a = []
 		
@@ -123,92 +216,103 @@ class Actor(object):
 		# For any missing colors, apply the default color
 		for color in xrange(len(self.points) - len(self.colors)):
 			a.append(self.default_color.data)
+		
+		# Call the base method
+		super(PrimitiveActor, self)._assignVBO(a)
+		
+	
+	def _render(self, stage, projection_matrix, modelCamera_matrix):
+		# Run the pre-render
+		shader = super(PrimitiveActor, self)._prerender(stage, projection_matrix, modelCamera_matrix)
+		
+		# Render
+		glDrawArrays(GL_TRIANGLES, 0, len(self.vbo) / 2)
+		
+		# Post render
+		super(PrimitiveActor, self)._postrender(stage, shader)
+		
+	
+	
+class TextActor(BaseActor):
+	'''
+	Actor for text
+	'''
+	# Actortype (used to determine shader)
+	actortype = 'text'
+
+	
+	def __init__(self, fontfile, size, text="", color=Color(1, 1, 1, 1)):
+		super(TextActor, self).__init__()
+		
+		# Text to render
+		self.text = text
 				
-		# Assign VBO
-		self.vbo = vbo.VBO(
-						   array(a, 'f')
-						   )
-		# Setup is complete
-		self.ready = True 
-			
+		# Text color
+		self.color = color
+		
+		# Create an OpenGL bitmap texture atlas of ascii glyphs  
+		self.atlas = Atlas(fontfile, size)
+		
+		a = self._getVertexData()		
+		# Assign the VBO
+		super(TextActor, self)._assignVBO(a)
+		
 	
-	
-	
-	def _render(self, stage, matrixstack):
+		
+	def _getVertexData(self):
 		'''
-		Render this Actor (to be called by stage!)
+		Get the vertex data for this object
 		'''
-		# Check if this object has been setup yet
-		if not self.ready:
-			self._updateVBO()
 				
-		# Push the current state of the matrix stack before we start changing things
-		matrixstack.push()
-		
-		# Apply transformations
-		matrixstack.translate(self.getTranslation())
-		matrixstack.rotatex(self.getRotation().x)
-		matrixstack.rotatey(self.getRotation().y)
-		matrixstack.rotatez(self.getRotation().z)
-		matrixstack.scale(self.getScale())	
-		
-		
-		# Calculate the camera2model matrix for this actor
-		modelCameraMatrix = matrixstack.top()
+		a = self.atlas
+		# All text objects are made at the origin
+		x = 0
+		y = 0
+				
+		coords = []
+		for p in self.text:
+			# Get char positions from charinfo
+			x2 = float(x) +  a.c[p].bl
+			y2 = float(-y) - a.c[p].bt
+			w = a.c[p].bw
+			h = a.c[p].bh
 			
-		# Render ourselves
-		self.vbo.bind()
-		try:
+			# Advance cursor to the start of the next character
+			x += a.c[p].ax
+			y += a.c[p].ay
 			
-			# Get our shader entry points
-			positionAttrib           = glGetAttribLocation(stage.shader, 'position')
-			colorAttrib              = glGetAttribLocation(stage.shader, 'color')
-			modelCameraMatrixUniform = glGetUniformLocation(stage.shader, 'modelToCameraMatrix');
-						
-			# Enable any vertex attribute arrays
-			glEnableVertexAttribArray(positionAttrib)
-			glEnableVertexAttribArray(colorAttrib)
+			# Skip glyphs that have no pixels
+			if w == 0 or h == 0:
+				continue
 			
-			# colorOffset is sizeof float (4) * floats per point (4) * num of points (vbo/2)
-			colorOffset = 4 * 4 * (len(self.vbo) / 2) 
-						
-			# Set the Attribute pointers			
-			glVertexAttribPointer(positionAttrib, 4, GL_FLOAT, False, 0, self.vbo)
-			glVertexAttribPointer(colorAttrib,    4, GL_FLOAT, False, 0, self.vbo + colorOffset)
-						
-			# Apply uniforms
-			glUniformMatrix4fv(modelCameraMatrixUniform, 1, GL_TRUE, modelCameraMatrix);
+			# Create points X, Y, Z, W
+			# X and Y are for the quad, Z/W are for the texture map
+			# These are split up in the vertex shader
+			# There are 6 points here, to make 2 tris 
+			coords.extend([x2    , -y2    , a.c[p].tx                           , a.c[p].ty            ]) 
+			coords.extend([x2 + w, -y2    , a.c[p].tx + (a.c[p].bw / float(a.w)), a.c[p].ty            ])
+			coords.extend([x2    , -y2 - h, a.c[p].tx						    , a.c[p].ty + (a.c[p].bh / a.h)])
+			coords.extend([x2 + w, -y2    , a.c[p].tx + (a.c[p].bw / float(a.w)), a.c[p].ty           ])
+			coords.extend([x2    , -y2 - h, a.c[p].tx						    , a.c[p].ty + (a.c[p].bh / a.h)])
+			coords.extend([x2 + w, -y2 - h, a.c[p].tx + (a.c[p].bw / float(a.w)), a.c[p].ty + (a.c[p].bh / a.h)])
 			
-			glDrawArrays(GL_TRIANGLES, 0, len(self.vbo) / 2)
-			
-			glDisableVertexAttribArray(positionAttrib)
-			glDisableVertexAttribArray(colorAttrib)
-			
-		finally:
-			self.vbo.unbind()
-			
-			
-		# Then render our children
-		for child in self.children:
-			child._render(stage, matrixstack)
+		return coords
+				
 
-			
-		# Pop the matrix stack back to what it was
-		matrixstack.pop()
-		pass
+		
+	def _render(self, stage, projection_matrix, modelCamera_matrix):
+		# Run the pre-render
+		shader = super(TextActor, self)._prerender(stage, projection_matrix, modelCamera_matrix)
+		
+		
+		# Update the values in the VBO
+		a = self._getVertexData()
+		self.vbo.set_array(array(a, 'f'))
+		
+		# Render
+		glDrawArrays(GL_TRIANGLES, 0, len(self.vbo) / 4)
+		
+		# Post render
+		super(TextActor, self)._postrender(stage, shader)
 
-
-	def computePositionOffsets(self, elapsedTime):
-		'''
-		Compute X,Y position offsets for the vertex shader
-		'''
-		loopDuration = 5.0
-		scale = 3.14159 * 2.0 / loopDuration;
 		
-		currTimeThroughLoop = float(elapsedTime) % loopDuration
-		
-		xOffset = cos(currTimeThroughLoop * scale) * 100.0;
-		yOffset = sin(currTimeThroughLoop * scale) * 100.0;
-		zOffset = 0
-		
-		return xOffset, yOffset, zOffset
