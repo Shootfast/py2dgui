0f3844911bb5f9f59a76b222c7ff7a0038cd9477
Mark Boorer <markboo99@gmail.com>
2012-11-23 10:26:05 +1100

    Added TextActor to namespace

--- python/py2dgui/__init__.py
+++ python/py2dgui/__init__.py
@@ -1,4 +1,4 @@
 from py2dgui.primitives import Triangle, Square, BorderedSquare
 from py2dgui.stage import Stage
 from py2dgui.base import Point, Color
-from py2dgui.actor import TextActor
+from py2dgui.actor import Group, ImageActor, TextActor 
