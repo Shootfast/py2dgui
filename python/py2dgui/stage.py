33c939755b1592d783a6b5f5ed6f84b59763776f
Mark Boorer <markboo99@gmail.com>
2012-11-22 11:33:58 +1100

    Added new shader object loading when shader is required

--- python/py2dgui/stage.py
+++ python/py2dgui/stage.py
@@ -1,17 +1,11 @@
-from py2dgui.shaders import vertex, fragment
+from py2dgui.shaders import shaderlist 
 from py2dgui.base import MatrixStack
 
-from OpenGL.GL import   shaders,			    \
-						GL_TRUE,                \
-						GL_VERTEX_SHADER,	    \
-						GL_FRAGMENT_SHADER,     \
-						GL_BLEND,               \
+from OpenGL.GL import	GL_BLEND,               \
 						GL_SRC_ALPHA,           \
 						GL_ONE_MINUS_SRC_ALPHA, \
 						glEnable,               \
-						glBlendFunc,            \
-						glGetUniformLocation,   \
-						glUniformMatrix4fv
+						glBlendFunc
 						
 from time import time
 
@@ -25,9 +19,12 @@ class Stage(object):
 		self.width = 1
 		self.height = 1
 		
+		# Actors on this stage
 		self.actors = []
 		
-		self.shader = None
+		self.shaders = {}
+		
+		# Store current matrixes
 		self.projection_matrix = None
 		self.modelCamera_matrix = MatrixStack()
 		
@@ -40,21 +37,34 @@ class Stage(object):
 		self.setup()
 		self.resize(width, height)
 		
+		
 	def setup(self):
 		'''
-		Compile the shader programs
-		'''
-		
-		# Compile the Vertex Shader
-		VERTEX_SHADER = shaders.compileShader(vertex, GL_VERTEX_SHADER)
-		# Compile the Fragment Shader
-		FRAGMENT_SHADER = shaders.compileShader(fragment, GL_FRAGMENT_SHADER)
-		# Join them as the shader program
-		self.shader = shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
-		
+		Setup the parts of the stage
+		'''		
 		# Ensure that transparency is enabled 
 		glEnable(GL_BLEND)
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+		
+		
+	def getShader(self, shadername):
+		'''
+		Check each actor to see if its shader 
+		'''
+		# If we already have the shader loaded, return it
+		if shadername in self.shaders.keys():
+			return self.shaders[shadername]
+		
+		
+		# Otherwise create a new instance of the shader
+		for shader in shaderlist:
+			if shader.name == shadername:
+				s = shader()
+				self.shaders[shadername] = s
+				return self.shaders[shadername]
+		
+		# Shader could not be found, raise exception	
+		raise Exception("Could not find shader that matched '%s'" % shadername)
 		
 		
 	def getElapsedTime(self):
@@ -102,22 +112,13 @@ class Stage(object):
 		'''
 		Render all actors on the stage
 		'''
+		
 		before = time()
-				
-		# Load our shader
-		shaders.glUseProgram(self.shader)
-		
-		# Set the projectionMatrix uniform
-		projectionMatrixUniform = glGetUniformLocation(self.shader, 'projectionMatrix');
-		glUniformMatrix4fv(projectionMatrixUniform, 1, GL_TRUE, self.projection_matrix);
-		
+							
 		# Render our actors
 		for actor in self.actors:
-			actor._render(self, self.modelCamera_matrix)
+			actor._render(self, self.projection_matrix, self.modelCamera_matrix)
 		after = time()
-		
-		# Unload our shader
-		shaders.glUseProgram(0)
 		
 		# record the framerate
 		framerate = after - before
